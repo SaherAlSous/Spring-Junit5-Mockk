cb15f115d09e1485520328724695d6553da992a6
Administrator <admin@example.com>
2023-03-30 19:19:12 +0300

    JUnit5 Extended with Spring extension

--- src/test/kotlin/com/in28minutes/mockk/mockkdemo/businesspak/SomeBusinessImplTest.kt
+++ src/test/kotlin/com/in28minutes/mockk/mockkdemo/businesspak/SomeBusinessImplTest.kt
@@ -4,20 +4,25 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@ExtendWith(SpringExtension::class)
 class SomeBusinessImplTest {
+
     companion object {
         @MockK
         private lateinit var dataService: DataService
         private lateinit var someBusinessImpl: SomeBusinessImpl
-    }
 
-    @BeforeEach
-    fun `Initialize Mockk`() {
-        dataService = mockk<DataService>()
-        someBusinessImpl = SomeBusinessImpl(dataService)
+        @JvmStatic
+        @BeforeAll
+        fun `Initialize Classes`() {
+            dataService = mockk<DataService>()
+            someBusinessImpl = SomeBusinessImpl(dataService)
+        }
     }
 
     @Test
